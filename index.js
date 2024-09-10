0ed6ae99c6ba01c079c3414cf83d07f4caa62029
Paulo Gabriel C. Júnior <otaimer@outlook.com>
2024-09-10 09:35:26 +0100

    aula-1

---
+++ index.js
@@ -0,0 +1,18 @@
+/**
+// hello world
+console.log("Hello world!") **/
+
+//arrays, objects
+
+let metas = ["mayk", "alo"];
+console.log(metas[0]);
+
+let meta = {
+    value: 'ler um livro por mês',
+    checked: false,
+};
+
+console.log(meta.value);
+
+//function
+const criarMeta = () => {};
