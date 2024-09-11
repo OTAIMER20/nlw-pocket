6bd665459b9022aefe2c8680bed899a0d8a4b836
Paulo Gabriel C. Júnior <otaimer@outlook.com>
2024-09-11 20:10:12 +0100

    metas realizadas

--- index.js
+++ index.js
@@ -44,6 +44,22 @@ const listarMetas = async () => {
     console.log('Meta(s) marcada(s) como concluída(s)')
 };
 
+const metasRealizadas = async () => {
+    const realizadas = metas.filter((meta) => {
+        return meta.checked
+    })
+
+    if(realizadas.length == 0){
+        console.log('Nenhuma meta realizadas :(')
+        return
+    }
+
+    await select({
+        message: "Metas realizadas",
+        choices: [...realizadas]
+    })
+}
+
 const start = async () => {
     while (true){
 
@@ -59,6 +75,10 @@ const start = async () => {
                     value: "listar"
                 },
                 {
+                    name: "Metas realizadas",
+                    value: "realizadas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -72,6 +92,9 @@ const start = async () => {
                 break;
             case "listar":
                 await listarMetas();
+                break;
+            case "realizadas":
+                await metasRealizadas();
                 break;
             case "sair":
                 return
